2d9daf00581faa38a02dfe5697c770cfa5698c82
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-10-20 21:40:44 +0000

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Don Bouncy on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
